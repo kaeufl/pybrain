1549059f6ea89a1b3c3a442e7ce9f84e29dcb019
Paul Kaeufl <kaeufl@geo.uu.nl>
2012-02-03 12:48:17 +0100

    removed debug message

--- pybrain/supervised/trainers/bfgs.py
+++ pybrain/supervised/trainers/bfgs.py
@@ -45,16 +45,11 @@ class BFGSTrainer(Trainer):
         def updateStatus(params):
             test_error = self.ds_test.evaluateModuleMSE(self.module)
             if self.epoch > 0 and test_error <= amin(self.test_errors):
-                print "opt weights updated"
-                print self.module.params[0]
                 self.optimal_params = self.module.params.copy()
                 self.optimal_epoch = self.epoch
             print "Epoch " + str(self.epoch) + ", E = " +\
                   str(self._last_err / self.ds.getLength())
             print "Test set error: " + str(test_error)
-            
-            if self.epoch > 0:
-                print self.optimal_params[0]
             
             self.train_errors.append(self._last_err / self.ds.getLength())
             self.test_errors.append(test_error)
