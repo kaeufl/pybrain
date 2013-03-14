fix a few issues with MDNs add MDNEnsemble allowing to average over multiple MDNs fix plotting issues

pybrain/auxiliary/mdn.py
@@ -31,5 +31,7 @@ def getError(Y, T, M, c):
 
 def phi(T, mu, sigma, c):
     dist = np.sum((T[:,None,:]-mu)**2, axis=2)
-    tmp = (1.0 / (2*np.pi*sigma)**(0.5*c)) * np.exp(- 1.0 * dist / (2 * sigma))
+    tmp = np.exp(- 1.0 * dist / (2 * sigma))
+    tmp[tmp < np.finfo('float64').eps] = np.finfo('float64').eps
+    tmp *= (1.0 / (2*np.pi*sigma)**(0.5*c))
     return np.maximum(tmp, np.finfo(float).eps)

pybrain/datasets/mdn.py
@@ -9,9 +9,21 @@ class MDNDataSet(SupervisedDataSet):
     _cdataset = None
     
     def __init__(self, inp, target, M, inp_transform=None, tgt_transform=None):
+        """
+        Initialize an MDN dataset.
+        @param inp: int or np.array, input data or input dimension
+        @param target: int or np.array, target data or target dimension
+        @param M: number of Gaussian kernels #TODO: remove this from dataset!
+        @param inp_transform: set of input transformations that have been
+        applied to the dataset #TODO: remove
+        @param tgt_transform: #TODO: remove
+        """
         SupervisedDataSet.__init__(self, inp, target)
         self.M = M
-        self.c = target
+        if np.isscalar(target):
+            self.c = target
+        else:
+            self.c = target.shape[1]
         self.inp_transform=inp_transform
         self.tgt_transform=tgt_transform
 

pybrain/structure/networks/mdn.py
@@ -1,4 +1,4 @@
-__author__ = 'Paul Kaeufl, kaeufl@geo.uu.nl'
+__author__ = 'Paul Kaeufl, p.j.kaufl@uu.nl'
 
 import numpy as np
 from pybrain.structure.networks import FeedForwardNetwork
@@ -6,9 +6,17 @@ from pybrain.structure import LinearLayer, TanhLayer, \
                               BiasUnit, FullConnection
 
 class MixtureDensityNetwork(FeedForwardNetwork):
-    """Mixture density network
+    """
+    Mixture density network
     """
     def __init__(self, M, c, *args, **kwargs):
+        """
+        Initialize an MDN with M kernels and output dimension c.
+        
+        If periodic is True, wrapped Gaussian kernels are used (e.g. as described 
+        in Bishop, Christopher M., and C. Legleye. "Estimating conditional 
+        probability densities for periodic variables." (1994): 641-648. )
+        """
         FeedForwardNetwork.__init__(self, *args, **kwargs)
         self.M = M
         self.c = c
@@ -16,6 +24,24 @@ class MixtureDensityNetwork(FeedForwardNetwork):
         # have XML serialization work properly
         self.argdict['c'] = c
         self.argdict['M'] = M
+        
+    def _p(self, t, alpha, mu, sigma):
+        phi = self._phi(t, mu, sigma)
+        return np.sum(alpha * phi, axis = 1)
+
+    def _phi(self, T, mu, sigma):
+        if T.ndim == 1:
+            T = T[None,:]
+        dist = np.sum((T[:,None,:]-mu[None,:,:])**2, axis=2)
+        tmp = np.exp(- 1.0 * dist / (2 * sigma))
+        tmp[tmp < np.finfo('float64').eps] = np.finfo('float64').eps
+        tmp *= (1.0 / (2*np.pi*sigma)**(0.5*self.c))
+        return np.maximum(tmp, np.finfo(float).eps)
+    
+    def getPosterior(self, x, t):
+        y = self.activate(x)
+        alpha, sigma, mu = self.getMixtureParams(y)
+        return self._p(t, alpha, mu, sigma)
 
     def softmax(self, x):
         return np.exp(x) / np.sum(np.exp(x), axis = 0)
@@ -34,11 +60,6 @@ class MixtureDensityNetwork(FeedForwardNetwork):
         tmp = np.maximum(np.sum(alpha * phi, 0), np.finfo(float).eps)
         return -np.log(tmp)
 
-    def _phi(self, T, mu, sigma):
-        dist = np.sum((T[None,:]-mu)**2, axis=1)
-        phi = (1.0 / (2*np.pi*sigma)**(0.5*self.c)) * np.exp(- 1.0 * dist / (2 * sigma))
-        return np.maximum(phi, np.finfo(float).eps)
-
     def getOutputError(self, y, t):
         alpha, sigma, mu = self.getMixtureParams(y)
         phi = self._phi(t, mu, sigma)
@@ -50,12 +71,31 @@ class MixtureDensityNetwork(FeedForwardNetwork):
         #dE_dy_sigma = - 0.5 * pi * (((t-mu)**2 / sigma) - self.c)
         #dE_dy_mu = pi * (mu - t) / sigma
         dE_dy_mu = pi[:,None] * (mu - t[None,:]) / sigma[:,None]
-
+        
         outerr = np.zeros(self.outdim)
         outerr[0:self.M] = dE_dy_alpha
         outerr[self.M:2*self.M] = dE_dy_sigma
         outerr[2*self.M:] = np.reshape(dE_dy_mu, (self.M*self.c))
         return outerr
+    
+    def _transferNetStructure(self, innet, outnet):
+        for m in innet.inmodules:
+            outnet.addInputModule(m)
+        for m in innet.outmodules:
+            outnet.addOutputModule(m)
+        for m in innet.modules:
+            outnet.addModule(m)
+
+        for clist in innet.connections.values():
+            for c in clist:
+                outnet.addConnection(c)
+
+        try:
+            outnet.sortModules()
+        except ValueError:
+            print "Network cannot be converted."
+            return None
+        return True
 
     def convertToFastNetwork(self):
         """ Attempt to transform the network into a fast network. If fast networks are not available,
@@ -66,78 +106,98 @@ class MixtureDensityNetwork(FeedForwardNetwork):
         except:
             print "No fast networks available."
             return None
-
         net = self.copy()
-
         cnet = _MixtureDensityNetwork(self.M, self.c)
-
-        for m in net.inmodules:
-            cnet.addInputModule(m)
-        for m in net.outmodules:
-            cnet.addOutputModule(m)
-        for m in net.modules:
-            cnet.addModule(m)
-
-        for clist in net.connections.values():
-            for c in clist:
-                cnet.addConnection(c)
-
-        try:
-            cnet.sortModules()
-        except ValueError:
-            print "Network cannot be converted."
-            return None
-
+        self._transferNetStructure(net, cnet)
         cnet.owner = cnet
-        #cnet.M = self.M
-        #cnet.c = self.c
         return cnet
 
     def convertToPythonNetwork(self):
         cnet = self.copy()
         net = MixtureDensityNetwork(self.M, self.c)
-
-        for m in cnet.inmodules:
-            net.addInputModule(m)
-        for m in cnet.outmodules:
-            net.addOutputModule(m)
-        for m in cnet.modules:
-            net.addModule(m)
-
-        for clist in cnet.connections.values():
-            for c in clist:
-                net.addConnection(c)
-
-        try:
-            net.sortModules()
-        except ValueError:
-            print "Network cannot be converted."
-            return None
-
+        self._transferNetStructure(cnet, net)
         net.owner = net
         return net
 
-#def buildMixtureDensityNetwork(di, H, dy, M, fast = False):
-#    net = MixtureDensityNetwork(M, dy)
-#    dy = (2+dy)*M
-#    net.addInputModule(LinearLayer(di, name = 'i'))
-#    net.addModule(TanhLayer(H, name = 'h'))
-#    net.addModule(BiasUnit('bias'))
-#    net.addOutputModule(LinearLayer(dy, name = 'o'))
-#    net.addConnection(FullConnection(net['i'], net['h']))
-#    net.addConnection(FullConnection(net['bias'], net['h']))
-#    net.addConnection(FullConnection(net['bias'], net['o']))
-#    net.addConnection(FullConnection(net['h'], net['o']))
-#    net.sortModules()
-#    if fast:
-#        net = net.convertToFastNetwork()
-#        net.sortModules()
-#    return net
+class PeriodicMixtureDensityNetwork(MixtureDensityNetwork):
+    nperiods = 7
+    
+    hist_errors = list()
+    hist_deriv = list()
+    
+    def _p(self, t, alpha, mu, sigma):
+        phi = np.zeros([len(t), self.M])
+        for l in range(-self.nperiods, self.nperiods+1):
+            phi += self._phi(t+l*2*np.pi, mu, sigma)
+        return np.sum(alpha * phi, axis = 1)
+    
+    def getError(self, y, t):
+        alpha, sigma, mu = self.getMixtureParams(y)
+        phi = np.zeros([self.M])
+        for l in range(-self.nperiods,self.nperiods+1):
+            phi += self._phi(t+l*2*np.pi, mu, sigma)
+        tmp = np.maximum(np.sum(alpha * phi, 0), np.finfo(float).eps)
+        self.hist_errors.append(-np.log(tmp))
+        return -np.log(tmp)
+    
+    def getOutputError(self, y, t):
+        alpha, sigma, mu = self.getMixtureParams(y)
+        
+        L = np.arange(-self.nperiods, self.nperiods+1)
+        chi = t[:, None] + L[None, :]*2*np.pi
+        
+        Phi = np.zeros([self.M, len(L)])
+        for lk in range(len(L)):
+            Phi[:, lk] = self._phi(chi[:,lk], mu, sigma)
+        
+        aphi = alpha * np.sum(Phi, axis=1)
+        pi = aphi / np.sum(aphi, 0)
+        _pi = alpha / np.sum(aphi, 0)
 
-def buildMixtureDensityNetwork(di, H, dy, M, fast = False):
+        dE_dy_alpha = alpha - pi
+        #dE_dy_sigma = - 0.5 * pi * np.sum(((np.sum((chi-mu[:,:,None])**2, axis=1) / sigma[:,None]) - self.c), axis=1)
+        _dE_dy_sigma = Phi * (np.sum((chi[None,:,:] - mu[:,:,None])**2, axis=1) / sigma[:, None] - self.c)
+        dE_dy_sigma = -0.5 * _pi * np.sum(_dE_dy_sigma, axis=1)
+
+        _dE_dy_mu = Phi[:,None,:] * ((mu[:,:,None] - chi[None, :, :]) / sigma[:,None,None])
+        dE_dy_mu = _pi[:,None] * np.sum(_dE_dy_mu, axis=2)
+
+        outerr = np.zeros(self.outdim)
+        outerr[0:self.M] = dE_dy_alpha
+        outerr[self.M:2*self.M] = dE_dy_sigma
+        outerr[2*self.M:] = np.reshape(dE_dy_mu, (self.M*self.c))
+        self.hist_deriv.append(outerr)
+        return outerr
+    
+    def convertToFastNetwork(self):
+        """ Attempt to transform the network into a fast network. If fast networks are not available,
+        or the network cannot be converted, it returns None. """
+        #TODO: this should make use of the base class' convertToFastNetwork
+        try:
+            from arac.pybrainbridge import _PeriodicMixtureDensityNetwork #@UnresolvedImport
+        except:
+            print "No fast networks available."
+            return None
+        net = self.copy()
+        cnet = _PeriodicMixtureDensityNetwork(self.M, self.c)
+        self._transferNetStructure(net, cnet)
+        cnet.owner = cnet
+        return cnet
+
+    def convertToPythonNetwork(self):
+        cnet = self.copy()
+        net = PeriodicMixtureDensityNetwork(self.M, self.c)
+        self._transferNetStructure(cnet, net)
+        net.owner = net
+        return net
+
+def buildMixtureDensityNetwork(di, H, dy, M, fast = False, periodic = False):
     if type(H) == int:
         H = [H]
-    net = MixtureDensityNetwork(M, dy)
+    if periodic:
+        net = PeriodicMixtureDensityNetwork(M, dy)
+    else:
+        net = MixtureDensityNetwork(M, dy)
     dy = (2+dy)*M
     net.addInputModule(LinearLayer(di, name = 'i'))
     net.addModule(BiasUnit('bias'))
@@ -156,19 +216,3 @@ def buildMixtureDensityNetwork(di, H, dy, M, fast = False):
         net = net.convertToFastNetwork()
         net.sortModules()
     return net
-    
-
-#def buildFastMixtureDensityNetwork(di, H, dy, M):
-#    net = FastMixtureDensityNetwork(di, H, dy, M)
-#    #
-#    #net.addInputModule(LinearLayer(di, name = 'i'))
-#    #net.addModule(TanhLayer(H, name = 'h'))
-#    #net.addModule(BiasUnit('bias'))
-#    #net.addOutputModule(LinearLayer(dy, name = 'o'))
-#    #net.addConnection(FullConnection(net['i'], net['h']))
-#    #net.addConnection(FullConnection(net['bias'], net['h']))
-#    #net.addConnection(FullConnection(net['bias'], net['o']))
-#    #net.addConnection(FullConnection(net['h'], net['o']))
-#    #net.sortModules()
-#    
-#    return net

pybrain/supervised/trainers/mdntrainer.py
@@ -2,22 +2,19 @@ __author__ = 'Paul Kaeufl, kaeufl@geo.uu.nl'
 
 import numpy as np
 from scg import SCGTrainer
-from time import time
+#from time import time
 from copy import copy
 from itertools import islice
 from IPython.parallel import Client
 from IPython.parallel.util import interactive
-from arac.pybrainbridge import _Network
-from arac.cppbridge import MDNTrainer as CMDNTrainer
-from pybrain.structure.modules import TanhLayer, BiasUnit
+from arac.pybrainbridge import _Network, _PeriodicMixtureDensityNetwork
+from arac.cppbridge import FastMDNTrainer, FastPeriodicMDNTrainer
+from pybrain.structure.modules import BiasUnit
 
 class MDNTrainer(SCGTrainer):
     """Minimise a mixture density error function using a scaled conjugate gradient
     algorithm.
     For details on the function of the Mixture Density Network see Bishop, 1995.
-
-    TODO: This can be considered as a workaround. The error function should really
-    not be part of the trainer or the network.
     """
     _cmdntrainer = None
     
@@ -88,8 +85,8 @@ class MDNTrainer(SCGTrainer):
         # since minit="random" sometimes gives centroids outside the 
         # target range
         [centroid, label] = kmeans2(t, self.module.M, minit='points')
-        cluster_sizes = np.maximum(np.bincount(label), 1) # avoid empty clusters
-        alpha = cluster_sizes.astype('float64')/np.sum(cluster_sizes)
+        cluster_sizes = np.maximum(np.bincount(label, minlength=self.module.M), 1) # avoid empty clusters
+        alpha = cluster_sizes.astype('float64') / np.sum(cluster_sizes)
         if (self.module.M > 1):
             # estimate variance from the distance to the nearest centre
             sigma = cdist(centroid, centroid)
@@ -132,7 +129,6 @@ class MDNTrainer(SCGTrainer):
                 error += trainer.module.getError(y, target)
         trainer._last_err = error
         trainer.module._setParameters(oldparams)
-        #print "f took %.6f" % (time()-t0)
         return error
 
     @staticmethod
@@ -152,18 +148,23 @@ class MDNTrainer(SCGTrainer):
                 trainer.module.backActivate(outerr)
         trainer.module._setParameters(oldparams)
         #print "df took %.6f" % (time()-t0)
+        #print trainer.module.derivs
         return 1 * trainer.module.derivs
     
-    def getFastTrainer(self, testset=None):
+    def getFastTrainer(self, validation_set=None):
         assert isinstance(self.module, _Network), "A fast network is required. Call MDN.convertToFast() first."
+        if isinstance(self.module, _PeriodicMixtureDensityNetwork):
+            CMDNTrainer = FastPeriodicMDNTrainer
+        else:
+            CMDNTrainer = FastMDNTrainer
         if not self._cmdntrainer:
-            if testset==None:
+            if validation_set==None:
                 self._cmdntrainer = CMDNTrainer(self.module.proxies.map[self.module], 
                                                 self.ds.getFastDataset())
             else:
                 self._cmdntrainer = CMDNTrainer(self.module.proxies.map[self.module], 
                                                 self.ds.getFastDataset(),
-                                                testset.getFastDataset())
+                                                validation_set.getFastDataset())
         return self._cmdntrainer
 
 #    @staticmethod

pybrain/tools/plotting/mdnplot.py
@@ -5,35 +5,89 @@ import matplotlib.pyplot as plt
 from matplotlib import mpl
 from scipy.stats import cumfreq
 from pybrain.auxiliary import mdn
-from nnutil.preprocessing import center, standardize, whiten
+from pybrain.structure.networks.mdn import PeriodicMixtureDensityNetwork
+#from nnutil.preprocessing import center, standardize, whiten
 
 class MDNPlotter():
     def __init__(self, module, ds):
         self.module = module
         self.ds = ds
-        self.update()
+        self.y = None
+#        self.update()
+        self.p = None
         self.tgts = ds.getTargets()
 
     def update(self):
         self.y = self.module.activateOnDataset(self.ds)
-        self.p = None
-
-    def _p(self, t, alpha, mu, sigma):
-        return np.sum(alpha * mdn.phi(t, mu, sigma, self.module.c), axis = 1)
+        #self.p = None
 
     def linTransform(self, x, mu, scale):
         return x*scale+mu
 
-    def plot1DMixture(self, t, alpha, mu, sigma, target = None, linewidth = 2.0):
+#    def plot1DMixture(self, t, alpha, mu, sigma, target = None, linewidth = 2.0):
+#        if t.ndim == 1:
+#            t = t[:, None]
+#        #p=self._p(t, alpha, mu, sigma)
+#        p=self.module._p(t, alpha, mu, sigma)
+#        plt.plot(t, p, linewidth=linewidth)
+#        if target:
+#            plt.vlines(target, np.max(p), 0, linewidth=linewidth)
+##            plt.vlines(target, plt.gca().get_ylim()[0], plt.gca().get_ylim()[1],
+##                       linewidth=linewidth)
+#        return p
+    
+    def plot1DMixture(self, x, t, target = None, linewidth = 2.0, 
+                      transform=None):
         if t.ndim == 1:
             t = t[:, None]
-        p=self._p(t, alpha, mu, sigma)
+        p = self.module.getPosterior(x, t)
+        if transform:
+            Dt0 = np.max(t) - np.min(t)
+            t = self.linTransform(t, transform['mean'], transform['scale'])
+            p /= (np.max(t) - np.min(t))/Dt0
         plt.plot(t, p, linewidth=linewidth)
         if target:
+            if transform:
+                target = self.linTransform(target, transform['mean'], 
+                                           transform['scale'])
             plt.vlines(target, np.max(p), 0, linewidth=linewidth)
-#            plt.vlines(target, plt.gca().get_ylim()[0], plt.gca().get_ylim()[1],
-#                       linewidth=linewidth)
         return p
+
+#    def plot1DMixtureForSample(self, sample, transform=None,
+#                               show_target_dist=False,
+#                               show_uniform_prior=False,
+#                               prior_range=None,
+#                               linewidth=2.0,
+#                               res=300,
+#                               target_dist_line_style='g:',
+#                               target_dist_linewidth=0.5):
+#        alpha, sigma, mu = self.module.getMixtureParams(self.y[sample])
+#        tgts = self.tgts
+#        if transform:
+#            sigma = sigma*transform['scale']**2
+#            mu = self.linTransform(mu, transform['mean'], transform['scale'])
+#            tgts = self.linTransform(tgts, transform['mean'], transform['scale'])
+#            if prior_range:
+#                prior_range = self.linTransform(prior_range, transform['mean'],
+#                        transform['scale'])
+#        if prior_range==None:
+#            prior_range=[np.min(tgts), np.max(tgts)]
+#        
+#        t = np.linspace(prior_range[0], prior_range[1], res)
+#        #if np.log10(np.max(t)) > 15:
+#            
+#        p = self.plot1DMixture(t, alpha, mu, sigma, tgts[sample], linewidth)
+#            
+#        if show_target_dist:
+#            [h, edges] = np.histogram(tgts, res, normed=True,
+#                                      range=(np.min(tgts), np.max(tgts)))
+#            plt.plot(edges[1:], h, target_dist_line_style)
+#        if show_uniform_prior:
+#            yprior = 1./(prior_range[1]-prior_range[0])
+#            plt.hlines(yprior, prior_range[0], prior_range[1], 'g', linewidth=linewidth)
+#        #plt.xlim((np.min(tgts), np.max(tgts)))
+#        #plt.ylim((0, plt.gca().get_ylim()[1]))
+#        return t, p
 
     def plot1DMixtureForSample(self, sample, transform=None,
                                show_target_dist=False,
@@ -43,32 +97,26 @@ class MDNPlotter():
                                res=300,
                                target_dist_line_style='g:',
                                target_dist_linewidth=0.5):
-        alpha, sigma, mu = self.module.getMixtureParams(self.y[sample])
         tgts = self.tgts
-        if transform:
-            sigma = sigma*transform['scale']**2
-            mu = self.linTransform(mu, transform['mean'], transform['scale'])
-            tgts = self.linTransform(tgts, transform['mean'], transform['scale'])
-            if prior_range:
-                prior_range = self.linTransform(prior_range, transform['mean'],
-                        transform['scale'])
         if prior_range==None:
             prior_range=[np.min(tgts), np.max(tgts)]
-        
         t = np.linspace(prior_range[0], prior_range[1], res)
-        #if np.log10(np.max(t)) > 15:
-            
-        p = self.plot1DMixture(t, alpha, mu, sigma, tgts[sample], linewidth)
-            
+        smpl = self.ds.getSample(sample)
+        p = self.plot1DMixture(smpl[0], t, smpl[1], linewidth, 
+                               transform=transform)
         if show_target_dist:
+            if transform:
+                tgts = self.linTransform(tgts, transform['mean'], transform['scale'])
             [h, edges] = np.histogram(tgts, res, normed=True,
                                       range=(np.min(tgts), np.max(tgts)))
             plt.plot(edges[1:], h, target_dist_line_style)
         if show_uniform_prior:
+            if transform:
+                prior_range = self.linTransform(prior_range, transform['mean'],
+                                                transform['scale'])
             yprior = 1./(prior_range[1]-prior_range[0])
             plt.hlines(yprior, prior_range[0], prior_range[1], 'g', linewidth=linewidth)
-        #plt.xlim((np.min(tgts), np.max(tgts)))
-        #plt.ylim((0, plt.gca().get_ylim()[1]))
+        
         return t, p
 
     def plotConditionalForSample(self, sample,
@@ -138,42 +186,74 @@ class MDNPlotter():
         plt.hist(dkl, 50)
         plt.xlabel('nats')
 
+#    def getPosterior(self, x, t):
+#        if self.p == None:
+#            self.p = []
+#            if len(t.shape) == 1:
+#                t = t[:, None]
+#            if len(x.shape) == 1:
+#                x = x[:, None]
+#            for xi in range(len(x)):
+#                y = self.module.activate(x[xi])
+#                alpha, sigma, mu = self.module.getMixtureParams(y)
+#                #tmp = self._p(t, alpha, mu, sigma)
+#                tmp = self.module._p(t, alpha, mu, sigma)
+#                self.p.append(tmp)
+#            self.p = np.array(self.p)
+#        return self.p
+
     def getPosterior(self, x, t):
-        if self.p == None:
+        if t.ndim == 1:
+            t = t[:, None]
+        if x.ndim == 1:
+            return self.module.getPosterior(x, t)
+        elif x.ndim==2 and self.p == None:
             self.p = []
-            if len(t.shape) == 1:
-                t = t[:, None]
-            if len(x.shape) == 1:
-                x = x[:, None]
             for xi in range(len(x)):
-                y = self.module.activate(x[xi])
-                alpha, sigma, mu = self.module.getMixtureParams(y)
-                tmp = self._p(t, alpha, mu, sigma)
-                self.p.append(tmp)
+                self.p.append(self.module.getPosterior(x[xi], t))
             self.p = np.array(self.p)
         return self.p
-
     
     def plotCenters(self, center = None, transform = None, interactive=False,
-                    colors=None, size=20, plot_all_centers=False, square=True,
-                    rasterized=False):
+                    colors=None, size=20, plot_all_centers=False, square=False,
+                    rasterized=False, minimum_gain=None, show_colorbar=False):
         """
         Plot true vs. predicted posterior means.
-        @param center:      plot the specified kernel
-        @param transform:   apply the specified linear transformation. Transform is
-                            a dict of the form {'mean' : float, 'scale' : float}
-        @param interactive: allow interactive selection of samples by clicking in the plot window
-        @param colors:      list of colors: must be of the same length as the number of patterns in the dataset
-                            or 'alpha': the alpha value of the specified kernel is shown on the color-axis 
-        @param size:        size of the dots in scatter plot
+        @param center:           plot the specified kernel
+        @param transform:        apply the specified linear transformation. Transform is
+                                 a dict of the form {'mean' : float, 'scale' : float}
+        @param interactive:      allow interactive selection of samples by clicking in the plot window
+        @param colors:           list of colors: must be of the same length as the number of patterns in the dataset
+                                 or one of: 
+                                 - 'alpha': the alpha value of the specified kernel is shown on the color-axis
+                                 - 'gain': the information gain (kl distance) of the according distribution                                     
+        @param size:             size of the dots in scatter plot
         @param plot_all_centers: plot all kernels and plot alpha on the color-axis
+        @param minimum_gain:     Discard patterns with an information gain below threshold.
         """
+        if self.y == None:
+            self.update() 
         alpha, sigma, mu = mdn.getMixtureParams(self.y, self.module.M, self.module.c)
-
         tgts = self.tgts
+        if isinstance(self.module, PeriodicMixtureDensityNetwork):
+            while np.any(mu > np.max(tgts)):
+                mu[mu > np.max(tgts)] -= 2*np.pi
+            while np.any(mu < np.min(tgts)):
+                mu[mu < np.min(tgts)] += 2*np.pi
         if transform:
             mu = self.linTransform(mu, transform['mean'], transform['scale'])
             tgts = self.linTransform(tgts, transform['mean'], transform['scale'])
+        
+        dKL = None
+        if minimum_gain:
+            dKL = self.getInformationGain()
+            idxs = dKL > minimum_gain
+            alpha = alpha[idxs]
+            sigma = sigma[idxs]
+            mu = mu[idxs]
+            tgts = tgts[idxs]
+            dKL = dKL[idxs]
+        
         if center != None:
             mu = mu[:, center]
         elif plot_all_centers==False:
@@ -204,6 +284,15 @@ class MDNPlotter():
                         cmap=cmap, edgecolor='none',
                         rasterized=rasterized
             )
+        elif colors=='gain':
+            cmap = mpl.colors.LinearSegmentedColormap.from_list('tmp', 
+                                                                [[.8,.8,.8],[0,0,0]])
+            if dKL is None:
+                dKL = self.getInformationGain()
+            plt.scatter(mu, tgts, picker=interactive, c=dKL, s=size, 
+                        cmap=cmap, edgecolor='none',
+                        rasterized=rasterized
+            )
         elif colors is not None:
             plt.scatter(mu, tgts, picker=interactive, c=colors, s=size, 
                         edgecolor='none',
@@ -217,6 +306,9 @@ class MDNPlotter():
         if interactive:
             f=plt.gcf()
             f.canvas.mpl_connect('pick_event', MDNPlotter.scatterPick)
+            
+        if show_colorbar:
+            plt.colorbar()
         plt.xlabel('Prediction')
         plt.ylabel('Target')
         
@@ -258,6 +350,8 @@ class MDNPlotter():
         TODO: Use the true mode rather than the kernel with the largest mixing
         coefficient.
         """
+        if self.y == None:
+            self.update()
         alpha, sigma, mu = mdn.getMixtureParams(self.y, self.module.M, self.module.c)
         #maxidxs = np.argmax(alpha, axis=1)
         maxidxs=self.getMaxKernel(alpha, sigma)
@@ -299,17 +393,15 @@ class MDNPlotter():
         prior and posterior distribution.
         """
         eps = np.finfo('float').eps
-        prior, x = np.histogram(self.tgts, nbins, density=True)
-        dx = np.abs(x[1]-x[0])
-        #import pdb; pdb.set_trace()
+        prior, t = np.histogram(self.tgts, nbins, density=True)
+        dt = np.abs(t[1]-t[0])
         if sample==None:
-            posterior = self.getPosterior(self.ds.getField('input'), x)[:,:-1]
+            posterior = self.getPosterior(self.ds.getField('input'), t)[:,:-1]
         else:
-            alpha, sigma, mu = self.module.getMixtureParams(self.y[sample])
-            posterior = self._p(x[:-1, None], alpha, mu, sigma)[None, :]
+            posterior = self.getPosterior(self.ds.getField('input')[sample], t)[None, :-1]        
         return np.sum(np.where(prior > eps, 
-                               posterior * np.log(posterior/(prior+eps)) * dx, 
+                               posterior * np.log(posterior/(prior+eps)) * dt, 
                                0), 
                       axis=1)
 
-        
+    